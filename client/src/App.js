e8b81fe52916ba73e92669d4cad9bf73f58ef781
benbasinski97@gmail.com <benbasinski97@gmail.com>
2018-01-13 13:59:22 -0800

    added multiple pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,40 @@ import React, { Component } from 'react';
 import './App.css';
 import Plot from './components/Plot';
 import Header from './components/Header';
+import Shootings from './components/Shootings';
+import Main from './components/Main';
 
-export class App extends Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 'main'
+    }
+  }
+
+  setPage(newPage) {
+    this.setState({page: newPage});
+  }
+
+
   render() {
+    let Content = '';
+    switch (this.state.page) {
+      case 'plot':
+        Content = Plot;
+        break;
+      case 'map':
+        Content = Shootings;
+        break;
+      default:
+        Content = Main;
+        break;
+    }
+
     return (
       <div className="App">
-        <Header />
-        <Plot />
+        <Header setPage={(newPage) => this.setPage(newPage)} />
+        <Content setPage={(newPage) => this.setPage(newPage)} />
       </div>
     );
   }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,7 +5,7 @@ export default class Header extends Component {
 
     render() {
         return (
-            <div>
+            <div onClick={() => this.props.setPage('main')}>
                 <h1>Project Clearwater</h1>
                 <p>Exposing the inequality in police encounters.</p>
                 <hr/>
