4c7f67a0b712b2a00eacd9d1e90aee9c211affe5
Nicholas Olds <oldsn@D-108-179-186-102.dhcp4.washington.edu>
2018-01-13 17:38:43 -0800

    close to finish

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 
+import './Main.css';
+
 export default class Main extends Component {
     render() {
         return (
-            <div>
+            <div className="parent">
+                <div className="par1" onClick={() => this.props.setPage('plot')} >
+                    <p>Racial Profiling</p>
+                </div>
+                <div className="par2" onClick={() => this.props.setPage('map') }> 
+                    <p>Police Shooting</p>
+                </div>
+                <div className="tri">
+
+                </div>
+            </div>
+
+
+
+            /*<div>
                 <button onClick={() => this.props.setPage('plot')} >Racial Profiling</button>
                 <button onClick={() => this.props.setPage('map')}>Police Shootings</button>
-            </div>
+            </div>*/
+
         );
     }
 }
